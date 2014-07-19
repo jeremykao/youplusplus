cd77a1587ba968c188cd7fb17270eb0e163f6b46
Jeremy Kao <jermsthekao@gmail.com>
2014-07-19 14:54:57 -0700

    added routes for high level users

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,1 +1,40 @@
 //Users.js
+var mongoose = require('mongoose'),
+    autoincrement = require('mongoose-auto-increment');
+
+module.exports = function(database){
+  var db = database;
+  autoincrement.initialize(database);
+
+  var Schema = mongoose.Schema;
+  var usersSchema = new Schema({
+    uid: Number,
+    fname: String,
+    lname: String,
+    oauthProvider: [String],
+    oauthUid: [String],
+    challenges: [Number],
+  });
+
+  usersSchema.plugin(autoincrement.plugin, {model: 'Users', field: 'uid', startAt: 1, incrementBy: 1});
+  var Users = mongoose.model('Users', usersSchema);
+
+  Users.newUser = function(newUser){
+    var user = new Users(newUser);
+    user.save(function(err){
+      if (err){
+        console.error.bind(err, 'adding new user error:')
+      }
+    });
+  };
+
+  Users.getUser = function(uid, callback){
+    return Users.find({'uid': uid}, callback);
+  };
+
+  Users.getAllUsers = function(callback){
+    return Users.find({}, callback);
+  };
+
+  return Users;
+};
--- api/routes.js
+++ api/routes.js
@@ -2,9 +2,6 @@
 var Types = require('hapi').Types;
 var mongoose = require('mongoose');
 
-/*** Models ***/
-var Users = require('./models/Users')
-
 /*** Open DB Connection ***/
 mongoose.connect('mongodb://localhost/youplusplus');
 var db = mongoose.connection;
@@ -13,16 +10,52 @@ db.once('open', function(){
   console.log('opened database connection');
 });
 
+/*** Models ***/
+var Users = require('./models/Users')(db);
+
 /*** Routes ***/
 module.exports = [
 	{
     path: '/',
 	  method: 'GET', 
-	  handler: test
-	}
+	  handler: defaultFunc
+	},
+  {
+    path: '/users',
+    method: 'GET',
+    handler: getAllUsers
+  },
+  {
+    path: '/users',
+    method: 'POST',
+    handler: createUser
+  },
+  {
+    path: '/users/{uid}',
+    method: 'GET',
+    handler: getSingleUser
+  }
 ];
 
+function defaultFunc(request, reply){
+  reply("Yup, you just hit the You++ API.");
+}
 
+function getAllUsers(request, reply){
+  Users.getAllUsers(function(err, users){
+    reply(users);
+  })
+}
+function createUser(request, reply){
+  Users.newUser(request.params.newUser);
+}
+function getSingleUser(request, reply){
+  Users.getUser(request.params.uid, function(err, user){
+    reply(user);
+  });
+}
 function test(request, reply){
-  reply('yolo swag to the max');
+  Users.getAllUsers(function(err, users){
+    reply(users);
+  });
 }

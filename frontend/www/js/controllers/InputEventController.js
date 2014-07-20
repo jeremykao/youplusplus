20b1d167f9cf88c9d1a72b417af75f5c8eefd778
Jeremy Kao <jermsthekao@gmail.com>
2014-07-20 03:10:16 -0700

    adding input event submission

--- frontend/www/js/controllers/InputEventController.js
+++ frontend/www/js/controllers/InputEventController.js
@@ -3,10 +3,11 @@ yppApp.controller('InputEventController', ['$scope', 'UserService', 'UserChallen
   $scope.eventObj = {
     userId: 0,
     challengeId: "",
-    data: 0,
+    data: null  ,
     description: "",
     date: Date.now()
   };
+  $scope.selectedChallenge = false;
   $scope.$watch('eventObj.userId', function(){
     if ($scope.eventObj.userId !== 0){
       UserChallengeResource.fetchUserChallenges({uid: $scope.eventObj.userId}, function(response){
@@ -16,8 +17,8 @@ yppApp.controller('InputEventController', ['$scope', 'UserService', 'UserChallen
           console.log($scope.challengeOptions);
           for (var i = 0; i < $scope.challengeOptions.length; ++i){
             if ($scope.challengeOptions[i].cid === $scope.eventObj.challengeId){
-              console.log("YOLOL");
               $scope.metric = $scope.challengeOptions[i].metric;
+              $scope.selectedChallenge = true;
             }
           }
         });
@@ -25,7 +26,8 @@ yppApp.controller('InputEventController', ['$scope', 'UserService', 'UserChallen
     }
   });
   $scope.eventObj.userId = UserService.getUid();
-  $scope.submitEvent = function(challengeId){
-    console.log(challengeId);
+
+  $scope.submitEvent = function(){
+    console.log($scope.eventObj);
   };
 }]);
